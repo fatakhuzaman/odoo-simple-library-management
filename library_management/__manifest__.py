b16a124c872cf13ccccdbe2f0f1b5831928c476d
fatakhuzaman <muhamadnurfatakhuzaman@gmail.com>
2025-09-12 09:49:57 +0700

    Initial commit Simple Library Management

---
+++ library_management/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Library Management",
+    "summary": "Library Sistem Management",
+    "author": "Fatakhuzaman",
+    "website": "fatakhuzaman.com",
+    "category": "Uncategorized",
+    "version": "1.0.0",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/library_book_views.xml",
+        "views/library_transaction_views.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": True,
+}
